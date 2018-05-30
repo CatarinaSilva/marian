7c29e9b6b8ef4e765e116225f446815f9048019d
CatarinaSilva <catarina.cruz.csilva@gmail.com>
2018-05-30 15:47:21 +0100

    Fix arguments for loader scorer

--- src/amun/cpu/decoder/guided_score.cpp
+++ src/amun/cpu/decoder/guided_score.cpp
@@ -71,7 +71,7 @@ void GuidedScorerLoader::Load(const God&) {
 ScorerPtr GuidedScorerLoader::NewScorer(const God &god, const DeviceInfo&) const {
   size_t tab = Has("tab") ? Get<size_t>("tab") : 0;
   std::string type = Get<std::string>("type");
-  return ScorerPtr(new GuidedScorer(god, name_, config_, tab));
+  return ScorerPtr(new GuidedScorer(god, name, config, tab));
 }
 
 BaseBestHypsPtr GuidedScorerLoader::GetBestHyps(const God &god, const DeviceInfo &deviceInfo) const {
